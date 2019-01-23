1856acb5dcd6a7970925a2a2d5b51f383a38646a
John Pisano <zdoomcentral@gmail.com>
2019-01-23 15:15:44 -0500

    Updated dir structure

--- cEngine/cEngine.h
+++ cEngine/cEngine.h
@@ -3,6 +3,6 @@
 //For use by cEngine applications
 
 #include <stdio.h>
-#include "src/Application.h";
-#include "src/Core.h";
+#include "src/Application.h"
+#include "src/Core.h"
 #include "src/EntryPoint.h"
